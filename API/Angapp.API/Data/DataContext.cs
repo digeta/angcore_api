f30d062f1dff8bff90ac546b21508a854ba2e5a2
digeta <semih.temel@gmail.com>
2022-12-14 00:28:39 +0300

    ..initialis API part

---
+++ API/Angapp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Angapp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Angapp.API.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Inspection> Inspections { get; set; }
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
